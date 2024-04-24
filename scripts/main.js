c553e5f1f0f5674dd94c3fa9e5f762fba06cdb42
raphapo <144717729+raphapo@users.noreply.github.com>
2024-04-24 18:39:14 +0000

    hi.html

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+//task 1
+console.log("task 1")
+for (i=0; i<11 ;i++) {
+    console.log(i*i);
+}
+//task 2
+console.log("task 2") 
+    for(i=6;i>0;i--){
+    console.log(i-1);
+       
+}
+console.log("blastoff")
+//task 3
+console.log("task 3")
+    for(i=0;i<=50;i+=2){
+        console.log(i)
+    }
+//task 4
+
+console.log("task 4")
+let total = 0;
+for(i=0;i<=100;i++){
+    total = total + i;
+}
+console.log(total);
+
+//task 5
+
+console.log("task 5")
+for(i=0;i<=10;i++){
+    console.log(i*(i-1))
+}
+
+
