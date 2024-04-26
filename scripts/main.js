1becf59b55c04b392397652af9c7fb122a53e847
raphapo <144717729+raphapo@users.noreply.github.com>
2024-04-26 19:39:27 +0000

    hi.html

--- scripts/main.js
+++ scripts/main.js
@@ -36,6 +36,15 @@ for (let i = 1; i < (x + 1); i++) {
 
 //task 6
 console.log("task 6 ")
+let arr = [3, 1, 4, 2, 5];
+for(let i = 0; arr.length; i++){
+    console .log(arr[i])
+
+}
+
+//task 7
+
+console.log("task 7")
 
 
 
