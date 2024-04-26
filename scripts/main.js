46232dae78dea216249d274e6df068b06bd14f19
raphapo <144717729+raphapo@users.noreply.github.com>
2024-04-26 18:24:59 +0000

    fix.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,25 @@
 //task 1
 console.log("task 1")
-for (i=0; i<11 ;i++) {
-    console.log(i*i);
+for (i = 0; i < 11; i++) {
+    console.log(i * i);
 }
 //task 2
-console.log("task 2") 
-    for(i=6;i>0;i--){
-    console.log(i-1);
-       
+console.log("task 2")
+for (i = 6; i > 0; i--) {
+    console.log(i - 1);
+
 }
 console.log("blastoff")
 //task 3
 console.log("task 3")
-    for(i=0;i<=50;i+=2){
-        console.log(i)
-    }
+for (i = 0; i <= 50; i += 2) {
+    console.log(i)
+}
 //task 4
 
 console.log("task 4")
 let total = 0;
-for(i=0;i<=100;i++){
+for (i = 0; i <= 100; i++) {
     total = total + i;
 }
 console.log(total);
@@ -28,12 +28,16 @@ console.log(total);
 
 console.log("task 5")
 let fact = 1;
-let x= 10;
-for(i=0;i<(x+1);i++){
-    fact=(fact*i);
+let x = 10;
+for (let i = 1; i < (x + 1); i++) {
+    fact = (fact * i);
+    console.log(fact);
 }
-console.log( fact);
-    
+
+//task 6
+console.log("task 6 ")
+
+
 
 
 
