c8f63f28f6308df7fee65aaf4ee84050bf744a0c
raphapo <144717729+raphapo@users.noreply.github.com>
2025-05-22 16:29:05 +0000

    concrete dries

---
+++ scripts/final.js
@@ -0,0 +1,14 @@
+let brightness = 0.5;
+function changeBrightness(newBrightness){
+    brightness +=1;
+    const elem = document.getElementById("concrete1");
+    elem.style.filter = `brightness(${brightness})`;
+    
+}
+function resetBrightness(newBrightness){
+    brightness =0.5;
+    const elem = document.getElementById("concrete1");
+    elem.style.filter = `brightness(${brightness})`;
+
+}
+
